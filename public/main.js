28bd531b8e5b9fe20541b7010564fed192c3f028
prgnl <alexfilinsky@gmail.com>
2021-01-19 16:47:22 +0100

    test

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 const cursor = document.getElementById("js--scene");
 
-let box = document.createElement("a-box");
+/*let box = document.createElement("a-box");
 box.setAttribute("width",1);
 box.setAttribute("height",1);
 box.setAttribute("depth",1);
@@ -8,6 +8,7 @@ box.setAttribute("color","tomato");
 box.setAttribute("position","-3 -2 -20");
 box.setAttribute("id","js--box2");
 cursor.appendChild(box);
+*/
 
 // animation="property: position; to: 1 8 -10; dur: 2000; easing: linear; loop: true
 
