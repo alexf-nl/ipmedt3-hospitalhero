a61c3f2afa310c0b60614b56ed23d4e37fab85aa
Daniekdegroot <43374493+Daniekdegroot@users.noreply.github.com>
2021-01-27 16:51:49 +0100

    d

--- public/js/conversation.js
+++ public/js/conversation.js
@@ -2,20 +2,22 @@ document.getElementById("dialoog").addEventListener("click", volgendetext);
 var bericht = 0;
 function volgendetext() {
   alert("Hello! I am an alert box!!");
-  if (bericht == 0) {
-    document.getElementById("dialoog").src = "../img/dialoog2.png";
-    bericht = bericht + 1
-    
-  }
+
+if (bericht == 2) {
+  document.getElementById("dialoog").src = "../img/dialoog4.png";
+  bericht = bericht + 1
+}
   if (bericht == 1) {
     document.getElementById("dialoog").src = "../img/dialoog3.png";
     bericht = bericht + 1\
 
-  }
-  if (bericht == 2) {
-    document.getElementById("dialoog").src = "../img/dialoog4.png";
+}
+  if (bericht == 0) {
+    document.getElementById("dialoog").src = "../img/dialoog2.png";
     bericht = bericht + 1
 
   }
+
+
 
 }
