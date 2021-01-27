ab6872c28b850191d68e39501894abd6abd827dd
Daniekdegroot <43374493+Daniekdegroot@users.noreply.github.com>
2021-01-27 20:41:48 +0100

    d

--- public/js/conversation.js
+++ public/js/conversation.js
@@ -1,18 +1,14 @@
 document.getElementById("dialoog").addEventListener("click", volgendetext);
 var bericht = 0;
 function volgendetext() {
-  if (bericht == 1) {
+  if (bericht == 0) {
   document.getElementById("dialoog").src = "../img/dialoog2.png";
   }
-  if (bericht == 2) {
+  if (bericht == 1) {
     document.getElementById("dialoog").src = "../img/dialoog3.png";
   }
-  if (bericht == 3) {
+  if (bericht == 2) {
   document.getElementById("dialoog").src = "../img/dialoog4.png";
   }
-
-
-
 bericht = bericht + 1
-
 }
