e757d95004359533b03bdcd12e5f6fe960ef9db9
prgnl <alexfilinsky@gmail.com>
2021-01-27 13:23:27 +0100

    Update heroTransformeren.js

--- public/js/heroTransformeren.js
+++ public/js/heroTransformeren.js
@@ -12,11 +12,9 @@ AFRAME.registerComponent("transformeren", {
   update:function(){
     this.el.addEventListener('click', () => {
         let entity = this.el;
-        entity.parentNode.removeChild(giraffe);
         //entity.parentNode.removeChild(entity);
         alert('Superheld giraaf komt eraan!');
-        entity.parentNode.appendChild(superHero);
-      //  entity.parentNode.setAttribute('gltf-model', '#supergiraffe');
+        entity.parentNode.setAttribute('gltf-model', '#supergiraffe');
 
       //  element = '<a-entity class="clickable" gesture-handler position="0 0 0" class="item" id="js--supergiraffe" gltf-model="#supergiraffe" scale="1 1 1"></a-entity>';
       //  element.appendChild(element);
