5da5f9a0397f6ececd4ff32f9a13e4f57a729868
prgnl <alexfilinsky@gmail.com>
2021-01-27 13:35:40 +0100

    test

--- public/js/heroTransformeren.js
+++ public/js/heroTransformeren.js
@@ -8,6 +8,8 @@ AFRAME.registerComponent("transformeren", {
   init:function() {
     const giraffe = document.getElementById('js--giraffe');
     const superHero = document.getElementById('js--superHero');
+    const entity1 = this.el[1];
+    entity1.removeChild(entity1);
 },
   update:function(){
     this.el.addEventListener('click', () => {
@@ -15,8 +17,8 @@ AFRAME.registerComponent("transformeren", {
         //entity.parentNode.removeChild(entity);
         alert('Superheld giraaf komt eraan!');
         entity.parentNode.removeChild(entity);
-        entity.parentNode.setAttribute('gltf-model', '#supergiraffe');
-        entity.parentNode.appendChild(entity);
+      //  entity.parentNode.setAttribute('gltf-model', '#supergiraffe');
+        entity1.parentNode.appendChild(entity1);
         //test
 
       //  element = '<a-entity class="clickable" gesture-handler position="0 0 0" class="item" id="js--supergiraffe" gltf-model="#supergiraffe" scale="1 1 1"></a-entity>';
