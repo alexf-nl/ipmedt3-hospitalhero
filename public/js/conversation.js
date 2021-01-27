199057f408d788762937d2c0d79b258c23c531cc
Daniekdegroot <43374493+Daniekdegroot@users.noreply.github.com>
2021-01-27 22:11:00 +0100

    d

--- public/js/conversation.js
+++ public/js/conversation.js
@@ -1,8 +1,23 @@
+// elements click eventes all blender models
+document.getElementById("js--bandje").addEventListener("click", bandjealert);
+document.getElementById("js--spuit").addEventListener("click", bandjealert);
+document.getElementById("js--giraffe").addEventListener("click", bandjealert);
+
 document.getElementById("dialoog").addEventListener("click", volgendetext);
-var bericht = 0;
+
+let collect_bandje = 0;
+
+
+
+// bericht geladen 1ste bericht
+
+let bericht = 0;
 function volgendetext() {
-  if (bericht == 0) {
+  // bericht geladen 2de bericht
   document.getElementById("dialoog").src = "../img/dialoog2.png";
+  while (collect_bandje == 0){
+    alert("we zijn nu in de while loop");
+    break
   }
   if (bericht == 1) {
     document.getElementById("dialoog").src = "../img/dialoog3.png";
@@ -11,4 +26,16 @@ function volgendetext() {
   document.getElementById("dialoog").src = "../img/dialoog4.png";
   }
 bericht = bericht + 1
+}
+
+
+
+
+
+
+
+
+function bandjealert(){
+    alert("Hello! I am an alert box!!");
+    document.getElementById('js--bandje').remove();
 }
