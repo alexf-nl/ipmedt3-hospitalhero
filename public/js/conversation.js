6de7106e1cb3af433acb58c5db1e2a3b4fe5e31f
Daniekdegroot <43374493+Daniekdegroot@users.noreply.github.com>
2021-01-27 22:56:08 +0100

    d

--- public/js/conversation.js
+++ public/js/conversation.js
@@ -10,45 +10,30 @@ let collect_bandje = 0;
 
 
 // bericht geladen 1ste bericht
-function hopop(){
+function bandje_loop(){
   document.getElementById("js--bandje").addEventListener("click", bandjealert);
-  if (collect_bandje == 1){
-    alert("HEB HET BANDJE GEVONDEN");
-  }else {
-    hopop();
-  }
+  bandje_loop();
 }
 
 function volgendetext() {
   // bericht geladen 2de bericht
   document.getElementById("dialoog").src = "../img/dialoog2.png";
-  hopop();
-  // while (collect_bandje == 0){
-  //   document.getElementById("js--bandje").addEventListener("click", bandjealert);
-  //   if (collect_bandje == 1){
-  //     alert("HEB HET BANDJE GEVONDEN");
-  //     break;
-  //   }
-  // }
-
+  bandje_loop();
+}
+function new_tekst(){
   alert("Ik ben uit de while loop");
   if (collect_bandje == 1) {
     document.getElementById("dialoog").src = "../img/dialoog3.png";
   }
-  if (bericht == 2) {
-  document.getElementById("dialoog").src = "../img/dialoog4.png";
-  }
-
 }
 
-
-
-
-
+  
 
 
 
 function bandjealert(){
     collect_bandje = collect_bandje + 1;
     document.getElementById('js--bandje').remove();
+    new_tekst();
+
 }
