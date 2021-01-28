e355778a50c2b8f4ffb039dedd2b7559575f3335
Daniekdegroot <43374493+Daniekdegroot@users.noreply.github.com>
2021-01-28 16:59:39 +0100

    d

--- public/js/heroTransformeren.js
+++ public/js/heroTransformeren.js
@@ -4,26 +4,26 @@ console.log('hero transformeren');
 
 
 
-AFRAME.registerComponent("transformeren", {
-  init:function() {
-    let giraffe = document.getElementById('js--giraffe');
-    let superHero = document.getElementById('js--supergiraffe');
-},
-  update:function(){
-
-    this.el.addEventListener('click', () => {
-      //giraffe.setAttribute('visible', 'false');
-        let entity = this.el;
-        entity.parentNode.removeAttribute('gltf-model');
-        alert('superheld komt aan!');
-        document.getElementById('js--supergiraffe').object3D.visible = true
-
-
-      });
-
-  },
-  tick:function(){
-  },
-  pause:function(){},
-  play:function(){}
-});
+// AFRAME.registerComponent("transformeren", {
+//   init:function() {
+//     let giraffe = document.getElementById('js--giraffe');
+//     let superHero = document.getElementById('js--supergiraffe');
+// },
+//   update:function(){
+//
+//     this.el.addEventListener('click', () => {
+//       //giraffe.setAttribute('visible', 'false');
+//         let entity = this.el;
+//         entity.parentNode.removeAttribute('gltf-model');
+//         alert('superheld komt aan!');
+//         document.getElementById('js--supergiraffe').object3D.visible = true
+//
+//
+//       });
+//
+//   },
+//   tick:function(){
+//   },
+//   pause:function(){},
+//   play:function(){}
+// });
