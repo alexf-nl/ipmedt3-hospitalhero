bf098dbdcfe8f5dfb893e458694f65c629be2f30
Daniekdegroot <43374493+Daniekdegroot@users.noreply.github.com>
2021-01-28 14:43:03 +0100

    Daniek

--- public/js/conversation.js
+++ public/js/conversation.js
@@ -1,36 +1,48 @@
-// elements click eventes all blender models
-
-// document.getElementById("js--spuit").addEventListener("click", bandjealert);
-// document.getElementById("js--giraffe").addEventListener("click", bandjealert);
-
-
-
-let collect_bandje = 0;
-
-
+// start spel pas tot dat de gebruiker op tekst ballon heeft geklikt
+function uitleg_spel(){
+  document.getElementById("dialoog").addEventListener("click", start_spel;
+  if (0 == 1) {} else{ uitleg_spel();}
+}
 function start_spel(){
+  document.getElementById("dialoog").src = "../img/2stebericht.png";
   document.getElementById("dialoog").addEventListener("click", bandje_loop);
   if (0 == 1) {} else{ start_spel();}
 }
-// bericht geladen 1ste bericht
+
+
+// loop per item om te blijven zoeken tot dat ze hem hebben gevonden + tekst om actie te ondernemen
 function bandje_loop(){
-  document.getElementById("dialoog").src = "../img/dialoog2.png";
-  document.getElementById("js--bandje").addEventListener("click", bandjealert);
+  document.getElementById("dialoog").src = "../img/1steopzoeknaardeband.png";
+  document.getElementById("js--bandje").addEventListener("click", bandje_collect;
   if (0 == 1) {} else{ bandje_loop();}
 }
 function watjes_loop(){
-  document.getElementById("js--bandje").addEventListener("click", bandjealert);
+  document.getElementById("dialoog").src = "../img/1steopzoeknaardewatje.png";
+  document.getElementById("js--watje").addEventListener("click", watjes_collect);
   if (0 == 1) {} else{ watjes_loop();}
 }
-
-
-
-function bandjealert(){
-    collect_bandje = collect_bandje + 1;
-    document.getElementById('js--bandje').remove();
-    document.getElementById("dialoog").src = "../img/dialoog3.png";
+function spuit_loop(){
+  document.getElementById("dialoog").src = "../img/1steopzoeknaardespuit.png";
+  document.getElementById("js--spuit").addEventListener("click", watjes_collect);
+  if (0 == 1) {} else{ spuit_loop();}
 }
 
 
+
+
+//delete element and compliment tekst voor gebruiker
+function bandje_collect(){
+    document.getElementById('js--bandje').remove();
+    watjes_loop();
+}
+
+function watjes_collect(){
+    document.getElementById('js--watje').remove();
+    spuit_loop()
+}
+function spuit_collect(){
+    document.getElementById('js--spuit').remove();
+    //next bericht/funtie
+}
 
 start_spel();
