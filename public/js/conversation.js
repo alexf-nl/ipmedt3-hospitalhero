23560c6c47447e6e915114570dd854f611359a3e
Daniekdegroot <43374493+Daniekdegroot@users.noreply.github.com>
2021-01-28 14:03:38 +0100

    d

--- public/js/conversation.js
+++ public/js/conversation.js
@@ -3,23 +3,23 @@
 // document.getElementById("js--spuit").addEventListener("click", bandjealert);
 // document.getElementById("js--giraffe").addEventListener("click", bandjealert);
 
-document.getElementById("dialoog").addEventListener("click", volgendetext);
+
 
 let collect_bandje = 0;
 
 
-
+function start_spel(){
+  document.getElementById("dialoog").addEventListener("click", volgendetext);
+  if (0 == 1) {} else{ start_spel();}
+}
 // bericht geladen 1ste bericht
 function bandje_loop(){
   document.getElementById("js--bandje").addEventListener("click", bandjealert);
-  if (collect_bandje == 1) {
-    alert("ik heb het bandje");
-  }
-  else{
-    bandje_loop();
-  }
-
+  if (0 == 1) {} else{ bandje_loop();}
 }
+
+
+
 
 function volgendetext() {
   // bericht geladen 2de bericht
@@ -29,14 +29,10 @@ function volgendetext() {
 
 function new_tekst(){
   if (collect_bandje == 1) {
+    // bericht geladen 3de bericht
     document.getElementById("dialoog").src = "../img/dialoog3.png";
   }
 }
-
-
-
-
-
 function bandjealert(){
     collect_bandje = collect_bandje + 1;
     document.getElementById('js--bandje').remove();
