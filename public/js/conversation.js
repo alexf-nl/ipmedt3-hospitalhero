eab10b5b9d75de860ded9308db3b5b0b66ad87d5
Daniekdegroot <43374493+Daniekdegroot@users.noreply.github.com>
2021-01-29 15:50:09 +0100

    d

--- public/js/conversation.js
+++ public/js/conversation.js
@@ -13,27 +13,32 @@ function start_spel(){
 function bandje_loop(){
   document.getElementById("dialoog").src = "../img/1steopzoeknaardeband.png";
   document.getElementById("js--bandje").addEventListener("click", bandje_collect);
+  setTimeout(() => console.log("wacht"), 500);
   if (0 == 1) {} else{ bandje_loop();}
 }
 function watjes_loop(){
   document.getElementById("dialoog").src = "../img/1steopzoeknaardewatje.png";
   document.getElementById("js--watje").addEventListener("click", watjes_collect);
+  setTimeout(() => console.log("wacht"), 500);
   if (0 == 1) {} else{ watjes_loop();}
 }
 function spuit_loop(){
   document.getElementById("dialoog").src = "../img/1steopzoeknaardespuit.png";
   document.getElementById("js--spuit").addEventListener("click", spuit_collect);
+  setTimeout(() => console.log("wacht"), 500);
   if (0 == 1) {} else{ spuit_loop();}
 }
 function pleister_loop(){
   document.getElementById("dialoog").src = "../img/1steopzoeknaardepleister.png";
   document.getElementById("js--pleister").addEventListener("click", pleister_collect);
+  setTimeout(() => console.log("wacht"), 500);
   if (0 == 1) {} else{ pleister_loop();}
 }
 function giraf_loop(){
   document.getElementById("dialoog").src = "../img/eennalaatstebericht.png";
 
   document.getElementById("dialoog").addEventListener("click", giraf2_loop);
+  
   if (0 == 1) {} else{ giraf_loop();}
 }
 function giraf2_loop(){
